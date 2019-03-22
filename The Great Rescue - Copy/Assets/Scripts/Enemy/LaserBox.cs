960b8859ddc81cefaa6e60dcddaa512364a74eeb
Alex Bugnar <alexbugnar@users.noreply.github.com>
2019-03-22 14:03:31 +0100

    V2

---
+++ The Great Rescue - Copy/Assets/Scripts/Enemy/LaserBox.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBox : MonoBehaviour
+{
+    AudioSource m_MyAudioSource;
+    public GameObject hurtbox;
+    public float time = 4.0f;
+    private float timeactual;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        timeactual = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeactual -= Time.deltaTime;
+        
+       
+        if (timeactual <= 2.0f)
+        {
+            
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+           
+        }
+        
+      
+        if (timeactual <= 0.0f)
+        {
+           
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            var hurtboxob = (GameObject)Instantiate(hurtbox);
+            hurtboxob.transform.position = gameObject.transform.position;
+            hurtboxob.transform.parent = gameObject.transform;
+
+            timeactual = time;
+        }
+
+    }
+}
---
+++ The Great Rescue - Copy/Assets/Scripts/Enemy/Shrek.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrek : MonoBehaviour
+{
+
+    private Vector2 position;
+    public float speed;
+    private float direction;
+
+    public float time = 5f;
+    private float timeelapsed=0;
+    AudioSource m_MyAudioSource;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_MyAudioSource = GetComponent<AudioSource>();
+        m_MyAudioSource.Play();
+        position = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("PlayerCharacter") == null)
+        {
+
+
+
+            Destroy(gameObject);
+        }
+        if (BgScroll.MoveBg == true)
+        { Destroy(gameObject); }
+
+
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(-200, transform.position.y), speed * Time.deltaTime);
+        timeelapsed += Time.deltaTime;
+        if(timeelapsed >= time)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
