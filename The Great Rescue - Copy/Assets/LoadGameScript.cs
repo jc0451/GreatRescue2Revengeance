92bac047b91c03eddcac4681a0c6a42731527c35
HampusPaulsen <47183982+HampusPaulsen@users.noreply.github.com>
2019-03-24 15:45:02 +0100

    Placeholder comic + Portrait health script

--- The Great Rescue - Copy/Assets/LoadGameScript.cs
+++ The Great Rescue - Copy/Assets/LoadGameScript.cs
@@ -15,7 +15,20 @@ public class LoadGameScript : MonoBehaviour
     {
         if (Input.GetKeyDown("r"))
         {
+
+            ScoreScript.ScoreValue = 0;
+            BodyCount.Goblin = 0;
+            BodyCount.Troll = 0;
+            BodyCount.Orc = 0;
+
+
+            PlayerScript.health = 5;
+            HealthScore.HealthValue = 5;
+            Application.LoadLevel(Application.loadedLevel);
+
             UnityEngine.SceneManagement.SceneManager.LoadScene("3. MainScene");
+
+
         }
     }
 }
